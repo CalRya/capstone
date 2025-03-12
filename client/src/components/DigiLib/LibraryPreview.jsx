64a3398466db6c048235ce22a581df41a1e21fae
CalRya <lindsaysalvacion110@gmail.com>
2025-03-12 11:57:52 +0800

    connect

--- client/src/components/DigiLib/LibraryPreview.jsx
+++ client/src/components/DigiLib/LibraryPreview.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-
+import "../CSS FOLDER/libraryPreview.css";
 
 const LibraryPreview = () => {
   const [books, setBooks] = useState([]);
